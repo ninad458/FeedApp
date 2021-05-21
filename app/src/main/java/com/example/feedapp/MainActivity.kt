321801ca3cd6cf321a2668354397d51978a0efce
enigma <ninadthakare458@gmail.com>
2021-05-22 03:01:06 +0530

    feed data viewmodel and activity recycler

--- app/src/main/java/com/example/feedapp/MainActivity.kt
+++ app/src/main/java/com/example/feedapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.feedapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel.fetchFeed()
+        val feedListView = findViewById<RecyclerView>(R.id.list_feed)
+        val feedAdapter = FeedAdapter()
+        feedListView.adapter = feedAdapter
+        feedListView.layoutManager = LinearLayoutManager(this)
+
+        viewModel.feed.observe(this, { feed ->
+            feedAdapter.setData(feed)
+        })
     }
 }
--- app/src/main/java/com/example/feedapp/MainViewModel.kt
+++ app/src/main/java/com/example/feedapp/MainViewModel.kt
@@ -1,4 +1,46 @@
 package com.example.feedapp
 
-class MainViewModel {
+import androidx.lifecycle.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val api: RetrofitService) : ViewModel() {
+
+    private val postsLiveData = MutableLiveData<List<String>>()
+    private val friendsLiveData = MutableLiveData<List<String>>()
+
+    val feed: LiveData<List<FeedListItem>> = MediatorLiveData<List<FeedListItem>>().apply {
+        fun prepareData() {
+            val posts = postsLiveData.value ?: return
+            val friends = friendsLiveData.value
+            val feedData = posts
+                .map { FeedListItem.PostType(it) }
+                .toMutableList<FeedListItem>()
+            if (friends != null && friends.isNotEmpty())
+                feedData.add(0, FeedListItem.FriendsType(friends))
+            postValue(feedData)
+        }
+
+        addSource(postsLiveData) {
+            prepareData()
+        }
+        addSource(friendsLiveData) {
+            prepareData()
+        }
+    }
+
+
+    fun fetchFeed() {
+        viewModelScope.launch {
+            val deferredPosts = async { api.getPosts() }
+            val deferredFriends = async { api.getFriends() }
+            val posts = deferredPosts.await()
+            postsLiveData.postValue(posts)
+            val friends = deferredFriends.await()
+            friendsLiveData.postValue(friends)
+        }
+    }
 }
