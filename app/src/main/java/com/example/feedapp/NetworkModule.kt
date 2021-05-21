5ddc8abb2d5c26d2efecd55aa84ce075bbcabc46
enigma <ninadthakare458@gmail.com>
2021-05-22 02:49:06 +0530

    network module di

--- app/src/main/java/com/example/feedapp/NetworkModule.kt
+++ app/src/main/java/com/example/feedapp/NetworkModule.kt
@@ -1,4 +1,30 @@
 package com.example.feedapp
 
+import androidx.annotation.WorkerThread
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import kotlinx.coroutines.delay
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
 object NetworkModule {
+
+    @Provides
+    fun provideRetrofitService(): RetrofitService {
+        return object : RetrofitService {
+            @WorkerThread
+            override suspend fun getPosts(): List<String> {
+                delay(3000)
+                return listOf("Post 1", "Post 2", "Post 3", "Post 4")
+            }
+
+            @WorkerThread
+            override suspend fun getFriends(): List<String> {
+                delay(5000)
+                return listOf("Friend 1", "Friend 2", "Friend 3", "Friend 4", "Friend 5")
+            }
+        }
+    }
 }
